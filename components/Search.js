154a0eb2facf115dc982ba948d495013e32f2cbf
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-08-07 18:37:25 +0100

    Change Search.js

--- components/Search.js
+++ components/Search.js
@@ -22,13 +22,6 @@ Search = React.createClass({
     },
 
   render: function() {
-      var styles = {
-          fontSize: '1.5em',
-          width: '90%',
-          maxWidth: '350px'
-      };
-
-  render: function() {
       var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
       return <input
