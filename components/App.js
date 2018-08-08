c4fcd933ed6c08ac24426ab0c67d24a69c668206
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-08-08 20:37:14 +0100

    Change components

--- components/App.js
+++ components/App.js
@@ -1,4 +1,44 @@
 App = React.createClass({
+
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+    getGif: function(searchingText, callback) {  // 1.
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+        var xhr = new XMLHttpRequest();  // 3.
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+               var data = JSON.parse(xhr.responseText).data; // 4.
+                var gif = {  // 5.
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);  // 6.
+            }
+        };
+        xhr.send();
+    },
+
+
+    handleSearch: function(searchingText) {  // 1.
+        this.setState({
+          loading: true  // 2.
+        });
+        this.getGif(searchingText, function(gif) {  // 3.
+          this.setState({  // 4
+            loading: false,  // a
+            gif: gif,  // b
+            searchingText: searchingText  // c
+          });
+        }.bind(this));
+    },
+
     render: function() {
 
         var styles = {
@@ -7,14 +47,7 @@ App = React.createClass({
             width: '90%'
         };
 
-        getInitialState() {
-            return {
-                loading: false,
-                searchingText: '',
-                gif: {}
-            };
-        },
-
+ 
         return (
             <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
@@ -25,6 +58,7 @@ App = React.createClass({
                 url={this.state.gif.url}
                 sourceUrl={this.state.gif.sourceUrl}
             />
+            <Search onSearch={this.handleSearch}/>
             </div>
         );
     }
--- components/Search.js
+++ components/Search.js
@@ -8,12 +8,12 @@ Search = React.createClass({
 
   handleChange: function(event) {
       var searchingText = event.target.value;
-      this.setState({searchingText: searchingText});
-
+      this.setState({
+        searchingText: searchingText});
       if (searchingText.length > 2) {
         this.props.onSearch(searchingText);
       }
-    }, // this.setState({searchingText: searchingText}); ??
+    }, 
 
   handleKeyUp: function(event) {
       if (event.keyCode === 13) {
@@ -32,40 +32,6 @@ Search = React.createClass({
               style={styles}
               value={this.state.searchTerm}
               />
-    //? return <Search onSearch={this.handleSearch}/>
-  } //value={this.state.searchTerm} ??
+    }
+  });
 
-  <Search onSearch={this.handleSearch}/>
-
-  handleSearch: function(searchingText) {  // 1.
-      this.setState({
-        loading: true  // 2.
-      });
-      this.getGif(searchingText, function(gif) {  // 3.
-        this.setState({  // 4
-          loading: false,  // a
-          gif: gif,  // b
-          searchingText: searchingText  // c
-        });
-      }.bind(this));
-    },
-
-    getGif: function(searchingText, callback) {  // 1.
-        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-        var xhr = new XMLHttpRequest();  // 3.
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data; // 4.
-                var gif = {  // 5.
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);  // 6.
-            }
-        };
-        xhr.send();
-    },
-
-
-});
